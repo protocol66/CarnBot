a5d32ba350315241e6fe4871f080494ec8bfbd80
jjsuperpower <jjs29356@gmail.com>
2020-01-25 22:12:44 -0600

    progress

--- CarnBot.py
+++ CarnBot.py
@@ -18,6 +18,13 @@ channels = {
 
 newUsrMsg = '''Welcome to the Easy Engineering server! Please say which course you are currently taking so a admin can update your role. If you have any questions regarding the server feel free to ask. Please read #rules. Remember we are all here to learn and help each so be respectful to your fellow students. \nDISCLAIMER: This bot is in no way affiliated with Dr. Carnal or TNTECH in anyway.'''
 
+modes = {
+		null: 'null',
+		sendMsg: 'sendmsg',
+		getRespons: 'getRespose'
+		
+}
+
 def is_me(m):
     return m.author == client.user
 
@@ -27,6 +34,7 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
+
 	#stops the bot from messaging when itself joins
 	if is_me(member):
 		return
@@ -45,21 +53,24 @@ async def on_message(message):
 	if is_me(message):
 		return
 
-	if message.content.startswith('$hello'):
-		await message.channel.send('Hello!')
+	if message.content.startswith('/'):
+		mode, message = botBrain(message)
 
-	if message.content.startswith(':thumbsup:'):
+		if (mode == modes[null]):
+			return
 
-		await message.channel.send('Send me that 👍 reaction, mate')
+		if (mode == modes[sendMsg]):
+			await message.channel.send(message)
 
-		def check(reaction, user):
-			return user == message.author and str(reaction.emoji) == '👍'
+		if (mode == modes[getResponse]):
+			try:
+        		reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
+        	except asyncio.TimeoutError:
+            	await channel.send('👎')
+        	else:
+            	await channel.send('👍')
 
-		try:
-			reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
-		except asyncio.TimeoutError:
-			await channel.send('👎')
-		else:
-			await channel.send('👍')
+
+
 
 client.run('NjY5MjYwOTU2NzY5MDU4ODY4.XitObg.CHG4AioEpx9sYHESfMsYaT_2bMM')
