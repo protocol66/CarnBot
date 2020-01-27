a85cd64b156cf98f10a66803855ec478064a0f22
jjsuperpower <jjs29356@gmail.com>
2020-01-26 22:39:07 -0600

    added some commands

--- CarnBot.py
+++ CarnBot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from discord.ext import commands
 import logging
 
 logger = logging.getLogger('discord')
@@ -8,7 +9,8 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-client = discord.Client()
+client = commands.Bot(command_prefix='$', help_command=None)
+TOKEN = 'NjY5MjYwOTU2NzY5MDU4ODY4.XitObg.CHG4AioEpx9sYHESfMsYaT_2bMM'
 
 channels = {
 		'general': 627271589062508566,
@@ -18,59 +20,73 @@ channels = {
 
 newUsrMsg = '''Welcome to the Easy Engineering server! Please say which course you are currently taking so a admin can update your role. If you have any questions regarding the server feel free to ask. Please read #rules. Remember we are all here to learn and help each so be respectful to your fellow students. \nDISCLAIMER: This bot is in no way affiliated with Dr. Carnal or TNTECH in anyway.'''
 
-modes = {
-		null: 'null',
-		sendMsg: 'sendmsg',
-		getRespons: 'getRespose'
-		
-}
 
+#basic function for checking if a messages author is the bot itself
 def is_me(m):
-    return m.author == client.user
+	return m.author == client.user
+
+async def sendMsg(channelToSend, message):
+	channel = client.get_channel(channels['aux1'])
+	await channel.send(message)
 
 @client.event
 async def on_ready():
 	print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_member_join(member):
+# @client.event
+# async def on_member_join(member):
 
-	#stops the bot from messaging when itself joins
-	if is_me(member):
-		return
+# 	#stops the bot from messaging when itself joins
+# 	if is_me(member):
+# 		return
 
-	print('Member called ' + member.name + ' joined')
-	try:
-		channel = client.get_channel(channels['aux1'])
-		await channel.send(member.mention + ' ' + newUsrMsg)
-		print('Sent welcome msg to ' + member.name)
-	except:
-		print('ERR: Could not send welcome msg.')
+# 	print('Member called ' + member.name + ' joined')
+# 	try:
+# 		channel = client.get_channel(channels['aux1'])
+# 		await channel.send(member.mention + ' ' + newUsrMsg)
+# 		print('Sent welcome msg to ' + member.name)
+# 	except:
+# 		print('ERR: Could not send welcome msg.')
 
-@client.event
-async def on_message(message):
-	print("Got message")
-	if is_me(message):
-		return
+# @client.event
+# async def on_message(message):
+# 	print("Got message")
+# 	if is_me(message):
+# 		return
 
-	if message.content.startswith('/'):
-		mode, message = botBrain(message)
+# 	if message.content.startswith('$'):
+		
+# 		if ('$echo' in message.content):
+# 			newMsg = message.content.replace('$echo', '')
+# 			await message.channel.send(newMsg)
 
-		if (mode == modes[null]):
-			return
+@client.command()
+async def echo(ctx, *, arg):
+    await ctx.send(arg)
 
-		if (mode == modes[sendMsg]):
-			await message.channel.send(message)
+@client.command()
+async def get(ctx, arg):
+	if (arg == 'help'):
+		await ctx.send('Arguments: email, website')
+	elif (arg == 'email'):
+		await ctx.send('CharlesLC@tntech.edu')
+	elif(arg == 'website'):
+		await ctx.send('https://clcee.net/clc_ece/')
+	else:
+		await ctx.send('I don\'t know what you are asking, read the syllabus')
 
-		if (mode == modes[getResponse]):
-			try:
-        		reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
-        	except asyncio.TimeoutError:
-            	await channel.send('👎')
-        	else:
-            	await channel.send('👍')
+@client.command()
+async def about(ctx):
+	await ctx.send('I am designed give some amusment, annoyance, and on rary occasions help to all jedi (EE/Compe) in training\nI am in no way affiliated with Dr. Charles Carnal')
+
+@client.command()
+async def help(ctx):
+	await ctx.send('Avalible commands:\n' +
+				   'About - gives general info about me, CarnBot\n' + 
+				   'get - quick way to get public info on Dr. Charles Carnal and his courses\n' +
+				   'echo - echo rest of message'
+				   )
 
 
+client.run(TOKEN)
 
-
-client.run('NjY5MjYwOTU2NzY5MDU4ODY4.XitObg.CHG4AioEpx9sYHESfMsYaT_2bMM')
