6e365a8fb1ea7690901e98a732398523ea0e7dd6
jjsuperpower <jjs29356@gmail.com>
2020-02-15 22:34:58 -0600

    cleaned up code ready to test

--- CarnBot.py
+++ CarnBot.py
@@ -6,6 +6,9 @@ import asyncio
 import random
 from datetime import datetime, date, time, timedelta
 
+###############
+###  setup  ###
+###############
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -13,29 +16,59 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-cmdSym = '$'
+# client = discord.Client()
+client = commands.Bot(command_prefix='$', help_command=None)
+
 channels = {
 		'general': 602663309170311173,
 		'C1-Q': 671814810278559774,
 		'C2-Q': 670503709989404732,
 }
-newUsrMsg = '''Welcome to the Easy Engineering server! Please say which course you are currently taking so a admin can update your role. If you have any questions regarding the server feel free to ask. Please read <#670503655308263434>. Remember we are all here to learn and help each so be respectful to your fellow students. \nDISCLAIMER: This bot is in no way affiliated with Dr. Carnal or TNTECH in anyway.'''
-now = datetime.today()
-resetTime = now.replace(day=now.day+1, hour=8, minute=0)
-general = None
 
-general = None
 
+############################
+###  constants and vars  ###
+############################
+
+# Globals Constants
+NEWUSRMSG = '''Welcome to the Easy Engineering server! Please say which course you are currently taking so a admin can update your role. If you have any questions regarding the server feel free to ask. Please read <#670503655308263434>. Remember we are all here to learn and help each so be respectful to your fellow students. \nDISCLAIMER: This bot is in no way affiliated with Dr. Carnal or TNTECH in anyway.'''
 RANDOM_MESSAGES_DAY = 2
+
+# Global Variables, Doc Brown look away
+# now = datetime.today()
+# RmdResetTime = now.replace(day=now.day + 1, hour=8, minute=0)
+# QtResetTime = 
+general = None
 messageTimes = []
 quotesSaid = 0
+
+###########################
+###  utility functions  ###
+###########################
+
+async def send_reminders(channel, today, date, testNum):
+	testNum = str(testNum)
+	dateDelta = date - today.date()
+	# print('checking to send reminder with dateDelta = ' + str(dateDelta))
+	dateDelta = dateDelta.days
+	if dateDelta == 14:
+		print('sending 2 week reminder')
+		await channel.send('REMINDER: TEST ' + testNum + ' is 2 weeks away!')
+	elif dateDelta == 7:
+		print('sending 1 week reminder')
+		await channel.send('REMINDER: TEST ' + testNum + ' is a week away! If you are still having trouble with some concepts, remember to ask questions or utilize tutoring.')
+	elif dateDelta == 3:
+		print('sending 3 day reminder')
+		await channel.send('REMINDER: TEST ' + testNum + ' is 3 days away! Utilize tutoring if you need it!!!')
+	elif dateDelta == 1:
+		print('sending 1 day reminder')
+		await channel.send("RED ALERT: TEST " + testNum + " is TOMORROW!!! If you haven't started studying yet, don't bother starting! Rethink what you are doing and start thinking of alternative majors.")
 
 def getQuotes():
 	with open('quotes.txt', 'r') as f:
 		text = f.read()
 		quotes = text.split('\n')
 	return quotes
-
 
 def getDates():
 	with open('important-dates.txt', 'r') as f:
@@ -59,23 +92,21 @@ def getDates():
 				dates.append(final)
 		return dates
 
-
-client = discord.Client()
-
-client = commands.Bot(command_prefix='$', help_command=None)
-
 def is_me(m):
 	return m.author == client.user
 
+
+#########################
+###  event functions  ###
+#########################
 
 @client.event
 async def on_ready():
 	print('We have logged in as {0.user}'.format(client))
 	global general  # NEED TO FIND ALTERNATIVE
 	general = discord.utils.find(lambda c: c.name == 'general', client.get_all_channels())
-	# client.loop.create_task(random_quote())
+	client.loop.create_task(random_quote())
 	client.loop.create_task(important_reminders())
-
 
 @client.event
 async def on_member_join(member):
@@ -88,59 +119,27 @@ async def on_member_join(member):
 		await asyncio.sleep(2)
 		# general = client.get_channel(channels['aux1'])
 		general = client.get_channel(channels['general'])
-		await general.send(member.mention + ' ' + newUsrMsg)
+		await general.send(member.mention + ' ' + NEWUSRMSG)
 		print('Sent welcome msg to ' + member.name)
 	except:
 		print('ERR: Could not send welcome msg.')
-
 
 @client.event
 async def on_message(message):
 	print("Got message")
 	if is_me(message):
 		return
-
-	if message.content.startswith('/hello'):
-		await message.channel.send('Hello!')
-
-	if message.content.startswith(cmdSym + 'final'):
-		dates = getDates()
-		if message.channel == client.get_channel(channels['C1-Q']):
-			await message.channel.send(message.author.mention + 'The final is on ' + str(dates[3][0]) + ' at ' + str(dates[3][1]) + ' to ' + str(dates[3][2]))
-		if message.channel == client.get_channel(channels['C2-Q']):
-			await message.channel.send(message.author.mention + 'The final is on ' + str(dates[7][0]) + ' at ' + str(dates[7][1]) + ' to ' + str(dates[7][2]))
-
-	if message.content.startswith(':thumbsup:'):
-    
-#for sum reason on_message prevents commands from running
-
 	#needed for commands to work
 	await client.process_commands(message)
 
+	#for testing
 	if message.content.startswith('/hello'):
 		await message.channel.send('Hello!')
 
-	if message.content.startswith(cmdSym + 'final'):
-		dates = getDates()
-		if message.channel == client.get_channel(channels['C1-Q']):
-			await message.channel.send(message.author.mention + 'The final is on ' + str(dates[3][0]) + ' at ' + str(dates[3][1]) + ' to ' + str(dates[3][2]))
-		if message.channel == client.get_channel(channels['C2-Q']):
-			await message.channel.send(message.author.mention + 'The final is on ' + str(dates[7][0]) + ' at ' + str(dates[7][1]) + ' to ' + str(dates[7][2]))
 
-	if message.content.startswith(':thumbsup:'):
-
-		await message.channel.send('Send me that 👍 reaction, mate')
-
-		def check(reaction, user):
-			return user == message.author and str(reaction.emoji) == '👍'
-
-		try:
-			reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
-		except asyncio.TimeoutError:
-			await message.channel.send('👎')
-		else:
-			await message.channel.send('👍')
-
+###########################
+###  command functions  ###
+###########################
 
 @client.command()
 async def echo(ctx, *, arg):
@@ -168,18 +167,27 @@ async def help(ctx):
 				   'get - quick way to get public info on Dr. Charles Carnal and his courses\n' +
 				   'echo - echo rest of message'
 				   )
-		try:
-			reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
-		except asyncio.TimeoutError:
-			await message.channel.send('👎')
-		else:
-			await message.channel.send('👍')
 
+@client.command()
+async def final(ctx):
+	dates = getDates()
+	if ctx.channel == client.get_channel(channels['C1-Q']):
+		await ctx.channel.send(ctx.author.mention + 'The final is on ' + str(dates[3][0]) + ' at ' + str(dates[3][1]) + ' to ' + str(dates[3][2]))
+	if ctx.channel == client.get_channel(channels['C2-Q']):
+		await ctx.channel.send(ctx.author.mention + 'The final is on ' + str(dates[7][0]) + ' at ' + str(dates[7][1]) + ' to ' + str(dates[7][2]))
+
+
+########################
+###  main functions  ###
+########################
 
 async def random_quote():
 	quotesSaid = 0
 	while True:
-		if quotesSaid < 2:
+		if quotesSaid < RANDOM_MESSAGES_DAY:
+			sleepTime = int(random.uniform(0, (8 / RANDOM_MESSAGES_DAY) * 60 * 60))
+			print(f"Random quote time = {sleepTime}")
+			await asyncio.sleep(sleepTime)
 			quotes = getQuotes()
 			try:
 				print('Sending quote.')
@@ -188,30 +196,11 @@ async def random_quote():
 			except:
 				print('EER: Failed sending quote.')
 			quotesSaid += 1
-			await asyncio.sleep(random.randint(3*60*60, 12*60*60))
 		else:
-			await discord.utils.sleep_until(resetTime)
+			now = datetime.today()
+			resetTime = now.replace(day=now.day+1, hour=8, minute=0)
+			discord.utils.sleep_until(resetTime)
 			quotesSaid = 0
-
-
-async def send_reminders(channel, today, date, testNum):
-	testNum = str(testNum)
-	dateDelta = date - today.date()
-	# print('checking to send reminder with dateDelta = ' + str(dateDelta))
-	dateDelta = dateDelta.days
-	if dateDelta == 14:
-		print('sending 2 week reminder')
-		await channel.send('REMINDER: TEST ' + testNum + ' is 2 weeks away!')
-	elif dateDelta == 7:
-		print('sending 1 week reminder')
-		await channel.send('REMINDER: TEST ' + testNum + ' is a week away! If you are still having trouble with some concepts, remember to ask questions or utilize tutoring.')
-	elif dateDelta == 3:
-		print('sending 3 day reminder')
-		await channel.send('REMINDER: TEST ' + testNum + ' is 3 days away! Utilize tutoring if you need it!!!')
-	elif dateDelta == 1:
-		print('sending 1 day reminder')
-		await channel.send("RED ALERT: TEST " + testNum + " is TOMORROW!!! If you haven't started studying yet, don't bother starting! Rethink what you are doing and start thinking of alternative majors.")
-
 
 async def important_reminders():
 	iDates = getDates()
@@ -258,6 +247,8 @@ async def important_reminders():
 					await C2_Channel.send("RED ALERT: FINAL is TOMORROW at " + str(Circuits2[3][1]) + " to " + str(Circuits2[3][2]) + "!!! If you haven't started studying yet, don't bother starting! Start getting prepared for taking this class again next semester.")
 		except:
 			print("ERR: Couldn't check and send test reminders.")
+		now = datetime.today()
+		resetTime = now.replace(day=now.day+1, hour=8, minute=0)
 		await discord.utils.sleep_until(resetTime)
 
 client.run('NjY5MjYwOTU2NzY5MDU4ODY4.XitObg.CHG4AioEpx9sYHESfMsYaT_2bMM')
