b79b1ee2a09c8a3606f1684060c8eab1f7c2ac9d
jjsuperpower <jjs29356@gmail.com>
2020-04-12 19:48:29 -0500

    update $echo command

--- CarnBot.py
+++ CarnBot.py
@@ -162,8 +162,27 @@ async def on_message(message):
 ###########################
 
 @client.command()
-async def echo(ctx, *, arg):
-	await ctx.send(arg)
+async def echo(ctx, *arg):
+
+	if (not len(arg) or arg[0] == "help"):
+		await ctx.send('Usage: del/delete <message>\n del/delete - deletes the original message')
+		return
+
+	delete = False
+
+	if (arg[0] == "del" or arg[0] == "delete"):
+		arg = arg[1:]
+		delete = True
+	
+	if(ctx.message.channel_mentions):
+		send_channel = ctx.message.channel_mentions[0]
+		arg = arg[1:]
+		await send_channel.send(" ".join(arg))
+	else:
+		await ctx.send(" ".join(arg))
+
+	if (delete):
+		await ctx.message.delete()
 
 
 @client.command()
