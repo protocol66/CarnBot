8855345de324febd3aa66a8dfcde6116ac85ddda
jjsuperpower <jjs29356@gmail.com>
2021-01-07 13:57:07 -0600

    finished pull

--- CarnBot.py
+++ CarnBot.py
@@ -251,7 +251,6 @@ async def shutdown(ctx):
 async def pull(ctx):
 	await ctx.send("Pulling From Github")
 	await ctx.send(os.popen(".././pull.sh").read())
-#rand comment
 
 @client.command()
 async def panic(ctx):
