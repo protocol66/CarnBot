30fba9fafc9722506fc417da5bd48a13f336a0c4
jjsuperpower <jjs29356@gmail.com>
2021-01-07 16:06:14 -0600

    done

--- CarnBot.py
+++ CarnBot.py
@@ -251,7 +251,7 @@ async def shutdown(ctx):
 async def pull(ctx):
 	await ctx.send("Pulling From Github")
 	await ctx.send(os.popen("./pull.sh").read())
-	#its NOT working
+	#its working
 
 @client.command()
 async def panic(ctx):
