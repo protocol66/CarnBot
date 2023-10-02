f5d6b9c0888cee2a3231e6c4498bc3bd471839be
Cameron <36605530+Invexh@users.noreply.github.com>
2023-10-02 13:10:37 -0400

    Update commands.py
    Added updated commands from master branch.

--- features/commands.py
+++ features/commands.py
@@ -122,16 +122,38 @@ async def testNewMemberMsg(ctx):
 async def nukeChats(ctx):
 	for channel in ctx.guild.channels:
 		if ((channel.name == "discussion") or (channel.name == "lab") or (channel.name == "past-work")):
-			await channel.clone()
 			await channel.delete()
+	for cat in ctx.guild.categories:
+		if "ECE" in cat.name:
+			await cat.delete()
+
+@client.command()
+async def createChats(ctx):
+	for role in ctx.guild.roles:
+		overwrites = {
+			ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+			role: discord.PermissionOverwrite(read_messages=True)
+		}
+
+		if ("ECE" in role.name):
+			category = await ctx.guild.create_category(role.name, overwrites=overwrites)
+			await ctx.guild.create_text_channel("discussion", category=category, overwrites=overwrites)
+			await ctx.guild.create_text_channel("past-work", category=category, overwrites=overwrites)
+			if("/" in role.name):
+				await ctx.guild.create_text_channel("lab", category=category, overwrites=overwrites)
 
 @client.command()
 async def clearRoles(ctx):
-    for member in ctx.guild.members:
-        for role in member.roles:
-            if (not (role.name in persistent_roles)):
-                try:
-                    await member.remove_roles(role)
-                except:
-                    logger.error("Failed to remove role " + role.name + " from " + member.name)
-
+	rolesWithPersistence = []
+	rolesWithPersistence.append(discord.utils.get(ctx.guild.roles, name="Alumni"))
+	rolesWithPersistence.append(discord.utils.get(ctx.guild.roles, name="Professor"))
+	rolesWithPersistence.append(discord.utils.get(ctx.guild.roles, name="Graduate Student"))
+	for member in ctx.guild.members:
+		if (any(r in member.roles for r in rolesWithPersistence)):
+			continue
+		for role in member.roles:
+			if (not (role.name in persistent_roles)):
+				try:
+					await member.remove_roles(role)
+				except:
+					print("Failed to remove role " + role.name + " from " + member.name)
