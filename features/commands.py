cf4740d2697147998a1af3236940f9468f67f262
jjsuperpower <jjs29356@gmail.com>
2023-10-11 21:20:52 -0500

    uncommented old functions

--- features/commands.py
+++ features/commands.py
@@ -118,59 +118,59 @@ async def echo(ctx:discord.context.ApplicationContext,
 # 			await channel.clone()
 # 			await channel.delete()
 
-@client.command()
-async def pull(ctx):
-	await ctx.send("Pulling From Github")
-	await ctx.send(os.popen("git pull").read())
-	#its working
+# @client.command()
+# async def pull(ctx):
+# 	await ctx.send("Pulling From Github")
+# 	await ctx.send(os.popen("git pull").read())
+# 	#its working
 
-@client.command()
-async def panic(ctx):
-	panicGifs = getPanicGIFS()
-	gif = panicGifs['GIFS'][random.randint(1, panicGifs['numGifs'])]
-	await ctx.channel.send(gif)
+# @client.command()
+# async def panic(ctx):
+# 	panicGifs = getPanicGIFS()
+# 	gif = panicGifs['GIFS'][random.randint(1, panicGifs['numGifs'])]
+# 	await ctx.channel.send(gif)
 
 
-@client.command()
-async def testNewMemberMsg(ctx):
-	await ctx.channel.send(NEWUSRMSG)
+# @client.command()
+# async def testNewMemberMsg(ctx):
+# 	await ctx.channel.send(NEWUSRMSG)
 
-@client.command()
-async def nukeChats(ctx):
-	for channel in ctx.guild.channels:
-		if ((channel.name == "discussion") or (channel.name == "lab") or (channel.name == "past-work")):
-			await channel.delete()
-	for cat in ctx.guild.categories:
-		if "ECE" in cat.name:
-			await cat.delete()
+# @client.command()
+# async def nukeChats(ctx):
+# 	for channel in ctx.guild.channels:
+# 		if ((channel.name == "discussion") or (channel.name == "lab") or (channel.name == "past-work")):
+# 			await channel.delete()
+# 	for cat in ctx.guild.categories:
+# 		if "ECE" in cat.name:
+# 			await cat.delete()
 
-@client.command()
-async def createChats(ctx):
-	for role in ctx.guild.roles:
-		overwrites = {
-			ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
-			role: discord.PermissionOverwrite(read_messages=True)
-		}
+# @client.command()
+# async def createChats(ctx):
+# 	for role in ctx.guild.roles:
+# 		overwrites = {
+# 			ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+# 			role: discord.PermissionOverwrite(read_messages=True)
+# 		}
 
-		if ("ECE" in role.name):
-			category = await ctx.guild.create_category(role.name, overwrites=overwrites)
-			await ctx.guild.create_text_channel("discussion", category=category, overwrites=overwrites)
-			await ctx.guild.create_text_channel("past-work", category=category, overwrites=overwrites)
-			if("/" in role.name):
-				await ctx.guild.create_text_channel("lab", category=category, overwrites=overwrites)
+# 		if ("ECE" in role.name):
+# 			category = await ctx.guild.create_category(role.name, overwrites=overwrites)
+# 			await ctx.guild.create_text_channel("discussion", category=category, overwrites=overwrites)
+# 			await ctx.guild.create_text_channel("past-work", category=category, overwrites=overwrites)
+# 			if("/" in role.name):
+# 				await ctx.guild.create_text_channel("lab", category=category, overwrites=overwrites)
 
-@client.command()
-async def clearRoles(ctx):
-	rolesWithPersistence = []
-	rolesWithPersistence.append(discord.utils.get(ctx.guild.roles, name="Alumni"))
-	rolesWithPersistence.append(discord.utils.get(ctx.guild.roles, name="Professor"))
-	rolesWithPersistence.append(discord.utils.get(ctx.guild.roles, name="Graduate Student"))
-	for member in ctx.guild.members:
-		if (any(r in member.roles for r in rolesWithPersistence)):
-			continue
-		for role in member.roles:
-			if (not (role.name in persistent_roles)):
-				try:
-					await member.remove_roles(role)
-				except:
-					print("Failed to remove role " + role.name + " from " + member.name)
+# @client.command()
+# async def clearRoles(ctx):
+# 	rolesWithPersistence = []
+# 	rolesWithPersistence.append(discord.utils.get(ctx.guild.roles, name="Alumni"))
+# 	rolesWithPersistence.append(discord.utils.get(ctx.guild.roles, name="Professor"))
+# 	rolesWithPersistence.append(discord.utils.get(ctx.guild.roles, name="Graduate Student"))
+# 	for member in ctx.guild.members:
+# 		if (any(r in member.roles for r in rolesWithPersistence)):
+# 			continue
+# 		for role in member.roles:
+# 			if (not (role.name in persistent_roles)):
+# 				try:
+# 					await member.remove_roles(role)
+# 				except:
+# 					print("Failed to remove role " + role.name + " from " + member.name)
